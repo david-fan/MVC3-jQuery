437e0e1af005715d769dd2292cd893500aa74afd
david-fan <david.bj@hotmail.com>
2013-04-18 18:16:41 +0800

    add sample class

---
+++ Data/MonoDBDriver.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace AddressBook_mvc3_jQuery
+{
+	public class MonoDBDriver
+	{
+		public MonoDBDriver ()
+		{
+			var connectionString = "mongodb://localhost";
+			var client = new MongoClient(connectionString);
+			var server = client.GetServer();
+			var database = server.GetDatabase("test");
+			var collection = database.GetCollection<Entity>("entities");
+			
+			var entity = new Entity { Name = "Tom" };
+			collection.Insert(entity);
+			var id = entity.Id;
+			
+			var query = Query<Entity>.EQ(e => e.Id, id);
+			entity = collection.FindOne(query);
+			
+			entity.Name = "Dick";
+			collection.Save(entity);
+			
+			var update = Update<Entity>.Set(e => e.Name, "Harry");
+			collection.Update(query, update);
+			
+			collection.Remove(query);
+		}
+	}
+
+	public class Entity
+	{
+		public ObjectId Id { get; set; }
+		public string Name { get; set; }
+	}
+
+}
+
